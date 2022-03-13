e56e3b22e7730cc706ee42f4efdafa04c9c011bc
Rémi Defoor <remi.defoor@student.howest.be>
2022-03-13 10:13:13 +0100

    create base service class

---
+++ app/Modules/Base/Services/Service.php
@@ -0,0 +1,45 @@
+<?php
+
+namespace App\Modules\Base\Services;
+
+use Illuminate\Database\Eloquent\Model;
+use Illuminate\Support\Facades\Validator;
+use Illuminate\Support\MessageBag;
+
+abstract class Service
+{
+    protected $model;
+    private $result;
+    private $errors;
+    protected $validationRules;
+
+    public function __construct(Model $model)
+    {
+        $this->model = $model;
+        $this->errors = new MessageBag();
+        $this->validationRules = [];
+    }
+
+    public function getResult()
+    {
+        return $this->result;
+    }
+
+    public function getErrors()
+    {
+        return $this->errors;
+    }
+
+    public function hasErrors()
+    {
+        return $this->errors->isNotEmpty();
+    }
+
+    protected function validate($data)
+    {
+        $this->errors = new MessageBag();
+
+        $validator = Validator::make($data, $this->validationRules);
+        if ($validator->fails()) $this->errors = $validator->errors();
+    }
+}
