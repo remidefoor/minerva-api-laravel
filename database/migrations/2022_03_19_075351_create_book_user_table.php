8d8d1f0d6986bff02d7b53505ea6b36e2c653771
Rémi Defoor <remi.defoor@student.howest.be>
2022-03-19 09:27:38 +0100

    create create book user table migration

---
+++ database/migrations/2022_03_19_075351_create_book_user_table.php
@@ -0,0 +1,34 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('book_user', function (Blueprint $table) {
+            $table->string('ISBN', 13);
+            $table->foreign('user_id')->references('id')->on('users')->onDelete('cascade');
+            $table->timestamps();
+
+            $table->primary(['ISBN', 'user_id']);
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('book_user');
+    }
+};
