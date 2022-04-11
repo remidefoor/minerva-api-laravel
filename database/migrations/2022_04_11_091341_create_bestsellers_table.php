8207f0cd4db1dbe7219dd6ceb726f739af49a4cb
Rémi Defoor <remi.defoor@student.howest.be>
2022-04-11 11:39:54 +0200

    create bestsellers migration

---
+++ database/migrations/2022_04_11_091341_create_bestsellers_table.php
@@ -0,0 +1,43 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('bestsellers', function (Blueprint $table) {
+            $table->id();
+            $table->integer('sold_copies');
+            $table->timestamp('created_at')->useCurrent();
+            $table->timestamp('updated_at')->useCurrent()->useCurrentOnUpdate();
+        });
+
+        Schema::create('bestsellers_language', function (Blueprint $table) {
+           $table->unsignedBigInteger('bestseller_id');
+           $table->foreign('bestseller_id')->references('id')->on('bestsellers')->onDelete('cascade');
+           $table->string('language', 2);
+           $table->string('title');
+           $table->timestamp('created_at')->useCurrent();
+           $table->timestamp('updated_at')->useCurrent()->useCurrentOnUpdate();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('bestsellers');
+        Schema::dropIfExists('bestsellers_language');
+    }
+};
