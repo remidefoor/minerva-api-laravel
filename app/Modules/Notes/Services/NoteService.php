cd45a34f0894c4e16c90884ee67d562240b07fce
Rémi Defoor <remi.defoor@student.howest.be>
2022-04-22 15:42:56 +0200

    add required services to note service

--- app/Modules/Notes/Services/NoteService.php
+++ app/Modules/Notes/Services/NoteService.php
@@ -4,15 +4,22 @@ namespace App\Modules\Notes\Services;
 
 use App\Modules\Base\Services\Service;
 use App\Modules\Notes\Models\Note;
+use App\Modules\UserBooks\Services\UserBookService;
+use App\Modules\Users\Services\UserService;
+use App\Modules\Validation\Models\Error;
 
 class NoteService extends Service
 {
+    private $userService;
+    private $userBookService;
     protected $validationRules = [
         'note' => ['string', 'required']
     ];
 
-    public function __construct(Note $model) {
+    public function __construct(Note $model, UserService $userService, UserBookService $userBookService) {
         parent::__construct($model);
+        $this->userService = $userService;
+        $this->userBookService = $userBookService;
     }
 
     public function getNotes($userId, $isbn) {
@@ -51,6 +58,12 @@ class NoteService extends Service
             ['user_id', $userId],
             ['ISBN', $isbn]
         ])->delete();
+    }
+
+    private function ensureNoteExists($userId, $isbn, $noteId) {
+        if (!$this->noteExists($userId, $isbn, $noteId)) {
+            $this->setError(new Error("A note with ID $noteId has not been found for the current user and book.", 404));
+        }
     }
 
     public function noteExists($userId, $isbn, $noteId) {
